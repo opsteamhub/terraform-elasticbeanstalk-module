3fa475dd6dd84ca73faa6a18c7d17577951b8514
Rafael Vinicius Cardoso Julio <rafael@ip-192-168-0-8.ec2.internal>
2023-07-11 16:43:51 -0300

    update terraform docs

--- variables.tf
+++ variables.tf
@@ -1,18 +1,23 @@
 variable "application" {
+  requeired   = yes
   description = "Application Name"
+  type        = string
   default     = "defaultapp"
 }
 
 variable "provisioned" {
+  type    = string
   default = "Terraform"
 }
 
 variable "owner" {
   description = "Project Owner"
+  type        = string
   default     = "OpsTeam"
 }
 
 variable "environment" {
+  required = yes
   description = "Custom Parameters for environment"
   type = map(object({
     solution_stack_name = optional(string, "64bit Amazon Linux 2 v3.5.9 running Docker")
